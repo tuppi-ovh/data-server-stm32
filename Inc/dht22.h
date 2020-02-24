29834102ba42f9ac2b2442637a072b3df77162e1
tuppi.ovh <tuppi.ovh@gmail.com>
2020-02-24 23:40:52 +0100

    first addings, not finished

---
+++ Inc/dht22.h
@@ -0,0 +1,33 @@
+/**
+ * @file dht22.h
+ * 
+ * Data Server STM32 - low level application for the Smart Home data acquisition.
+ * Copyright (C) 2020 Vadim MUKHTAROV
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * For information on Data Server STM32: tuppi.ovh@gmail.com
+ */
+
+#ifndef DHT22_H_
+#define DHT22_H_
+
+#include <stdint.h>
+#include "stm32f1xx_hal.h"
+
+void DHT22_Init(TIM_HandleTypeDef * htim_us, GPIO_TypeDef * gpio_port, int32_t gpio_pin);
+int32_t DHT22_AnalyseData(uint32_t * buffer, int32_t len, uint32_t * temper, uint32_t * rh);
+void DHT22_StartSensor(void);
+
+#endif /* DHT22_H_ */
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# data-server-stm32
+# Introduction
+
+TBD.
+
+# External Code Sources
+
+To compile the whole project you need to download manually:
+
+- External/Drivers/CMSIS (check [STM32CubeF1](https://www.st.com/content/st_com/en/products/embedded-software/mcu-mpu-embedded-software/stm32-embedded-software/stm32-standard-peripheral-libraries/stsw-stm32054.html#tools-software))
+
+- External/Drivers/STM32F1xx_HAL_Driver
+
+- External/Startup/startup_stm32f100xb.s
+
+# License 
+
+Refer to the LICENSE file.
+
+# Links 
+
+- https://www.controllerstech.com/temperature-measurement-using-dht22-in-stm32 
